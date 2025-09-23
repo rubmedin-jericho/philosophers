40520289767cce2bff1aff091e3f334cd92ee57b
rubmedin-jericho <rubenmedinamartinez2016@gmail.com>
2025-09-23 10:42:55 +0200

    push

--- philoshophers_main.c
+++ philoshophers_main.c
@@ -12,28 +12,45 @@
 
 #include "philoshophers.h"
 
-int	is_num(char c)
+int	is_sign_dup(char c, int *flag)
 {
 	if(c < '0' || c > '9')
-		reuturn (1);
+		*flag += 1;
+	if(*flag > 1)
+		return (1);
 	return (0);
+}
+
+int	is_num(char c)
+{
+	if((c >= '0' || c == ('-' || '+')) || (c <= '9' || c == ('-' || '+')))
+		return (0);
+	return (1);
 }
 
 int	check_errors(char **av)
 {
 	int	count_i;
 	int	count_j;
+	int	flag;
 
-	i = 0;
+	count_i = 1;
 	while(av[count_i])
 	{
 		count_j = 0;
-		while(av[count_j])
+		flag = 0;
+		while(av[count_i] [count_j])
 		{
 			if(is_num(av[count_i][count_j]))
+			{ 	
+				printf("ERROR NO NUM\n");
 				return (ERROR);
-			else if(is_dup(av[count_i] [count_j]))
+			}	
+			else if(is_sign_dup(av[count_i] [count_j], &flag))
+			{
+				printf("ERROR DUPLICADO\n");
 				return (ERROR);
+			}
 			count_j++;
 		}
 		count_i++;
@@ -44,8 +61,12 @@ int	check_errors(char **av)
 int main(int ac, char **av)
 {
 	if(ac != 6)
+	{
+		printf("FEW ARGUMENTS\n");
 		return (1);
+	}
 	if(check_errors(av))
 		return (1);
+	printf("FUNCIONA\n");
+	return (0);
 }
-return (0);
